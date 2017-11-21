d2983679750f83d5195db41423caaa77b2b1a4ab
sushma <sushma.desemsetty@raybiztech.com>
2017-11-21 15:39:34 +0530

    commited

---
+++ SampleApplication/SampleApplication/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using SampleApplication.Models;
+
+namespace SampleApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        MVCPractiseEntities objEntities = new MVCPractiseEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserData objUser)
+        {
+      UserData user=   objEntities.UserDatas.Find(objUser.username);
+            if (user.username == objUser.username && user.password == objUser.password)
+            {
+                return View("result", user);
+            }
+            ViewBag.message = "UserName or Password are incorrect";
+            return View(ViewBag.message);
+        }
+    }
+}
