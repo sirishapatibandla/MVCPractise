7974bd49721dae5aab6c9bb804e194a72d1134ca
sushma <sushma.desemsetty@raybiztech.com>
2017-11-24 18:28:47 +0530

    datab

--- CascadingDropdownlistApplication/CascadingDropdownlistApplication/Controllers/DataController.cs
+++ CascadingDropdownlistApplication/CascadingDropdownlistApplication/Controllers/DataController.cs
@@ -52,6 +52,7 @@ namespace CascadingDropdownlistApplication.Controllers
                     //{
 
                     //    oledbConn.Close();
+
                     //}
                 }
             }
